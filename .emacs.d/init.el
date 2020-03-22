88259b3341bd261bc540c46825452098be2b4c69
nikyq <kunwoo3526@gmail.com>
2020-03-22 18:44:16 +0900

    Remove rtags from leaf-all (silly commit duh)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -87,7 +87,7 @@
 (exec-path-from-shell-initialize)
 (leaf-keywords-init)
 (leaf-all counsel swiper god-mode evil-god-state hydra flycheck-haskell
-          lsp-mode company-lsp lsp-ui rtags)
+          lsp-mode company-lsp lsp-ui)
 
 ;; ------------------------------------------------------------------------------------
 ;; GLOBAL CONFIGS
